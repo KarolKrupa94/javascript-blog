6eb80caff2c475c544a61a59e512f0dff3088c15
Karol Krupa <darksoldier1397@gmail.com>
2022-04-05 21:28:48 +0200

    5.4 done & bug fixed

--- js/script.js
+++ js/script.js
@@ -47,41 +47,41 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-function generateTitleLinks(){
+function generateTitleLinks(customSelector = ''){
 
-    /* remove contents of titleList */
-    const optTitleListSelector = document.querySelector(titleList)
-    function clearMessages(){
-    document.getElementById('messages').innerHTML = '';
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector)
+  titleList.innerHTML = '';
+  
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+  let html = '';
+  for(let article of articles){
+    
+    /* get the article id */
+
+    const articleId = article.getAttribute('id')
+
+    /* find the title element */
+
+
+
+    /* get the title from the title element */
+    
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleId + '</span></a></li>';
+    html = html + linkHTML;
     }
-    /* for each article */
+    /* insert link into titleList */
+    titleList.innerHTML = html;
+    const links = document.querySelectorAll('.titles a');
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    } 
 
-    const articles = document.querySelectorAll(optArticleSelector);
-    let html = '';
-    for(let article of articles){
-    
-      /* get the article id */
-
-      const articleId = article.getAttribute('id')
-
-      /* find the title element */
-
-      /* get the title from the title element */
-    
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
-      /* create HTML of the link */
-    
-      const linkHTML = '<li><a href="#'+ articleId+'"><span>'+ articleId +'</span></a></li>';
-
-
-      /* insert link into titleList */
-
-      html = html + linkHTML;
   }
-
-  titleList.innerHTML = html;
-
-}
-
   generateTitleLinks();

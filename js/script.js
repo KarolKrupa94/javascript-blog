c10eda1d077d6de94a965645c5ed91399b3cf75f
Karol Krupa <darksoldier1397@gmail.com>
2022-04-05 20:46:48 +0200

    5.4 first commit

--- js/script.js
+++ js/script.js
@@ -42,3 +42,46 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+    /* remove contents of titleList */
+    const optTitleListSelector = document.querySelector(titleList)
+    function clearMessages(){
+    document.getElementById('messages').innerHTML = '';
+    }
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(let article of articles){
+    
+      /* get the article id */
+
+      const articleId = article.getAttribute('id')
+
+      /* find the title element */
+
+      /* get the title from the title element */
+    
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* create HTML of the link */
+    
+      const linkHTML = '<li><a href="#'+ articleId+'"><span>'+ articleId +'</span></a></li>';
+
+
+      /* insert link into titleList */
+
+      html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+
+}
+
+  generateTitleLinks();
